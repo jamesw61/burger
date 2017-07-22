e4b3cebf0490f657dfb22d7d27f1bdc867efac2d
James Wick <jmwick61@gmail.com>
2017-07-22 13:27:57 -0700

    schema

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mushroom', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chili', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('swiss', false);
+
+
